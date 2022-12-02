1ceee29e01174fb1756756f7833a0a9c5837bccf
Brittany Keith <bkinux@gmail.com>
2022-12-02 10:48:11 +0100

    Add coding challenge for Dec 2, 2022

---
+++ 8_kyu/02-12-22/app.js
@@ -0,0 +1,5 @@
+//Create a function that takes and integer as an argument and returns 'even' for evens and 'odd' for odds
+
+function even_or_odd(number){
+    return (number%2 == 0 ? 'even' : 'odd')
+}
