cde5c12722fabbb62ce91a8672128e15eb42912a
Brittany Keith <bkinux@gmail.com>
2023-06-29 13:15:42 +0200

    coding challenge for 06-29-23

---
+++ 8_kyu/06-29-23/main.js
@@ -0,0 +1,18 @@
+// Triple Trouble
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+// Note: You can expect all of the inputs to be the same length.
+//P: takes in three strings. 
+//R: return a string of the strings in order
+//E: aa, bb, cc --> abcabc
+//P: need to get into each string and map str 1-?? to a new string 
+
+function tripleTrouble(one, two, three){
+    let x = '';
+    for(i=0; i<one.length; i++){
+        x += one[i] + two[i] + three[i]
+    }
+    return x;
+   }
